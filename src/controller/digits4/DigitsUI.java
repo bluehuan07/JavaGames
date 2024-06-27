4952c3bce03c716873af2ead94fde15e293ab258
bluehuan07 <bluehuan07@gmail.com>
2024-06-27 10:20:20 +0800

    v0.0.3: feat 完成猜數字功能

--- src/controller/digits4/DigitsUI.java
+++ src/controller/digits4/DigitsUI.java
@@ -4,15 +4,20 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
+import controller.PickUI;
 import util.DispositionUI;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class DigitsUI extends JFrame {
 
@@ -22,6 +27,11 @@ public class DigitsUI extends JFrame {
 	private JTextField textFA;
 	private JTextField textFB;
 	private JTextField textFC;
+
+	int A2B;
+	int max = 9 * 9 * 8 * 7, min = 1023;
+	int[] allans = new int[max];
+	int i = 0, k = 0, tmp1;
 
 	/**
 	 * Launch the application.
@@ -54,11 +64,14 @@ public class DigitsUI extends JFrame {
 		contentPane.setLayout(gbl_panel);
 		setContentPane(contentPane);
 
+		creatallnumber();
+		max = allans.length - 1;
+
 		JPanel fackPane1 = new JPanel();
 		contentPane.add(fackPane1, DispositionUI.setGridBagConstraint(0.25, 0.25, 0, 1, 1, 1));
 		JPanel fackPane2 = new JPanel();
 		contentPane.add(fackPane2, DispositionUI.setGridBagConstraint(0.25, 0.25, 3, 1, 1, 1));
-		JLabel lblNewLabel_1 = new JLabel("New label");
+		JLabel lblNewLabel_1 = new JLabel("我猜是：" + allans[0]);
 		contentPane.add(lblNewLabel_1, DispositionUI.setGridBagConstraint(0.25, 0.25, 1, 0, 2, 1));
 		JLabel lblNewLabel_2 = new JLabel("A：");
 		contentPane.add(lblNewLabel_2, DispositionUI.setGridBagConstraint(0.25, 0.25, 1, 1, 1, 1));
@@ -89,6 +102,56 @@ public class DigitsUI extends JFrame {
 		tGBC.fill = GridBagConstraints.HORIZONTAL;
 		contentPane.add(textFC, tGBC);
 
+		JButton btnNewButton = new JButton("確認");
+		tGBC = DispositionUI.setGridBagConstraint(0.25, 0.25, 3, 3, 1, 1);
+		contentPane.add(btnNewButton, tGBC);
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int tmp = allans[0];
+				i = 0;
+				int inputA = Integer.parseInt(textFA.getText());
+				int inputB = Integer.parseInt(textFB.getText());
+				do {
+					if(inputA+inputB>4) {
+						JOptionPane.showMessageDialog(DigitsUI.this, "輸入錯誤");
+						break;
+					}
+					if(inputA==4) {
+						JOptionPane.showMessageDialog(DigitsUI.this, "答案是："+ tmp +"\n我很厲害吧~~~");
+						JOptionPane.showMessageDialog(DigitsUI.this, "遊戲結束");
+						PickUI frame = new PickUI();
+						frame.setVisible(true);
+						dispose();
+					}
+					if(max==0) {
+						JOptionPane.showMessageDialog(DigitsUI.this, "你484騙我\n請重新開始");
+						DigitsUI frame = new DigitsUI();
+						frame.setVisible(true);
+						dispose();
+					}
+					if (AB(allans[max], tmp, inputA, inputB)) {
+						while (AB(allans[i], tmp, inputA, inputB)) {
+							i++;
+						}
+						allans[i] = allans[max];
+					}
+					max--;
+				} while (max > i);
+				lblNewLabel_1.setText("我猜是：" + allans[0]);
+			}
+		});
+	}
+
+	public void creatallnumber() {
+		do {
+			allans[i] = 1023 + k;
+			if (testnuber(allans[i])) {
+				i++;
+			}
+			k++;
+			tmp1 = i - 1;
+		} while (allans[tmp1] != 9876);
 	}
 
 	public static boolean testnuber(int input) {
